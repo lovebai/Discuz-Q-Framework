60c40c344e00726e18241cf3cf612caf20c1e28e
流火行者 <coralchu@tencent.com>
2021-09-08 16:41:46 +0800

    no message

--- src/Api/ApiServiceProvider.php
+++ src/Api/ApiServiceProvider.php
@@ -104,38 +104,42 @@ class ApiServiceProvider extends ServiceProvider
 
     protected function populateRoutes(RouteCollection $route)
     {
-        $uri = $_SERVER['REQUEST_URI'];
-        $uri = str_replace('/', '', $uri);
-        if (strpos($uri, 'api') === 0) {
-            $route->group('/api', function (RouteCollection $route) {
-                require $this->app->basePath('routes/api.php');
-            });
-            $route->group('/api/backAdmin', function (RouteCollection $route) {
-                require $this->app->basePath('routes/apiadmin.php');
-            });
-        } else if (strpos($uri, 'apiv3') === 0) {
+        $reqUri = '';
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $reqUri = $_SERVER['REQUEST_URI'];
+            $uri = str_replace('/', '', $reqUri);
+        }
+        $route->group('/api', function (RouteCollection $route) {
+            require $this->app->basePath('routes/api.php');
+        });
+        if (strpos($uri, 'apiv3') === 0) {
             $route->group('/apiv3', function (RouteCollection $route) {
                 require $this->app->basePath('routes/apiv3.php');
             });
-        } else if (strpos($uri, 'plugin') === 0 && strpos($uri, 'api')) {
-            $plugins = Utils::getPluginList();
-            $originUrl = $_SERVER['REQUEST_URI'];
-            preg_match("/(?<=plugin\/).*?(?=\/api)/", $originUrl, $m);
-            $pluginName = $m[0];
-            $route->group('/api', function (RouteCollection $route) {
-                require $this->app->basePath('routes/api.php');
+        } else if (strpos($uri, 'api') === 0) {
+            $route->group('/api/backAdmin', function (RouteCollection $route) {
+                require $this->app->basePath('routes/apiadmin.php');
             });
-            foreach ($plugins as $plugin) {
-                if (strtolower($pluginName) == strtolower($plugin['name_en'])) {
-                    $route->group('/plugin/' . $plugin['name_en'] . '/api/', function (RouteCollection $route) use ($plugin) {
-                        $routes = $plugin['routes'];
-                        foreach ($routes as $name => $info) {
-                            $method = strtolower($info['method']);
-                            $route->$method($name, $plugin['name_en'] .'.'. str_replace('/', '.', $name), $info['controller']);
-                        }
-                    });
-                    break;
-                }
+        } else if (strpos($uri, 'plugin') === 0 && strpos($uri, 'api')) {
+            $this->setPluginRoutes($route, $reqUri);
+        }
+    }
+
+    private function setPluginRoutes($route, $reqUri)
+    {
+        $plugins = Utils::getPluginList();
+        preg_match("/(?<=plugin\/).*?(?=\/api)/", $reqUri, $m);
+        $pluginName = $m[0];
+        foreach ($plugins as $plugin) {
+            if (strtolower($pluginName) == strtolower($plugin['name_en'])) {
+                $route->group('/plugin/' . $plugin['name_en'] . '/api/', function (RouteCollection $route) use ($plugin) {
+                    $routes = $plugin['routes'];
+                    foreach ($routes as $name => $info) {
+                        $method = strtolower($info['method']);
+                        $route->$method($name, $plugin['name_en'] . '.' . str_replace('/', '.', $name), $info['controller']);
+                    }
+                });
+                break;
             }
         }
     }
