a88c157c47b4cbbe5c671d3c402bfcbb1defc198
wentguo <1553690085@qq.com>
2021-10-29 14:41:32 +0800

    商品插件调整： 插件appid传递

--- src/Api/ApiServiceProvider.php
+++ src/Api/ApiServiceProvider.php
@@ -18,6 +18,7 @@
 
 namespace Discuz\Api;
 
+use App\Common\Utils;
 use Discuz\Api\Controller\AbstractSerializeController;
 use Discuz\Api\Events\ApiExceptionRegisterHandler;
 use Discuz\Api\Events\ConfigMiddleware;
@@ -149,6 +150,7 @@ class ApiServiceProvider extends ServiceProvider
         $prefix = '/plugin/' . $plugin['name_en'] . '/api/';
         $route->group($prefix, function (RouteCollection $route) use ($plugin) {
             $pluginFiles = $plugin['plugin_' . $plugin['app_id']];
+            Utils::setAppKey("plugin_appid",$plugin['app_id']);
             if (isset($pluginFiles['routes'])) {
                 foreach ($pluginFiles['routes'] as $routeFile) {
                     require_once $routeFile;
